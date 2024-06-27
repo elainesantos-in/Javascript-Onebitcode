a75203ad12bcf404441fc044787b67a155fa6798
Elaine Santos <101003900+elainesantos-in@users.noreply.github.com>
2024-06-27 18:43:56 -0300

    exercicios

--- Javascript Mod V/Exercicios IA/Exer de Classes/Produto.js
+++ Javascript Mod V/Exercicios IA/Exer de Classes/Produto.js
@@ -2,13 +2,35 @@
 Objetivo: Criar uma classe básica com métodos e instanciar um objeto.
 Descrição: Crie uma classe Produto com propriedades nome, preco, e quantidade. 
 Adicione um método descrever que retorna uma string descrevendo o produto.
-Instancie um objeto da classe Produto e imprima a descrição do produto.*/
+Instancie um objeto da classe Produto e imprima a descrição do produto.
+
+Exercício 4: Encapsulamento
+Objetivo: Usar métodos getter e setter para controlar o acesso às propriedades.
+Descrição: Modifique a classe Produto para encapsular a propriedade preco com 
+métodos getter e setter, garantindo que o preço não possa ser negativo.
+
+*/
+
+
 
 module.exports = class Produto {
     constructor(nome,preco,quantidade){
         this.nome = nome
         this.preco = preco
         this.quantidade = quantidade
+    }
+
+    get validandoPreco(){
+        if(this.preco < 0 ){
+            let aviso = `O preço invalido`
+            return aviso
+        }else{
+            return this.preco
+        }
+    }
+
+    set validPreço(validandoPreco){
+        return validandoPreco
     }
 
     descrever(){
