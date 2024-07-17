8a5cb313f20307f8d11b2c2bb953e43a837aae7a
Elaine Santos <101003900+elainesantos-in@users.noreply.github.com>
2024-07-16 21:56:41 -0300

    exercicio

--- Javascript Mod V/25-exercicio22/entities/Usuario.js
+++ Javascript Mod V/25-exercicio22/entities/Usuario.js
@@ -1,3 +1,14 @@
 /*
 Usuario: Deve possuir atributos para nome completo, email e conta.
 */
+const Conta = require("./Conta")
+
+module.exports = class Usuario {
+  constructor(email, nomeCompleto) {
+    this.email = email
+    this.nomeCompleto = nomeCompleto
+    this.conta = new Conta(this)
+  }
+}
+
+
