e568e98a16d570b555c4503c329e3b9ed5c5326d
Elaine Santos <101003900+elainesantos-in@users.noreply.github.com>
2024-07-16 21:56:38 -0300

    exercicio

--- Javascript Mod V/25-exercicio22/entities/Transferencia.js
+++ Javascript Mod V/25-exercicio22/entities/Transferencia.js
@@ -1,18 +1,13 @@
 //Transfer: Deverá possuir atributos para: o usuário que enviou a transferência, o que recebeu a transferência, o valor e a data de criação.
 
 module.exports = class Transferencia {
-   constructor(nomeRemetente, nomeDestinatario, valor, data){
-        this.nomeRemetente = nomeRemetente
-        this.nomeDestinatario = nomeDestinatario
-        this.valor = valor
-        this.data = data
-    }
-
-    novaTransferencia(nomeRemetente, nomeDestinatario, valor){
-        let transfer = `Remetente: ${this.nomeRemetente} 
-        valor: ${this.valor}
-        data: ${this.data}
-        destinatario: ${this.nomeDestinatario}`
-        return transfer
+  constructor(deUsuario, paraUsuario, valor) {
+    this.deUsuario = deUsuario;
+    this.paraUsuario = paraUsuario;
+    this.valor = valor;
+    this.criadoEm = new Date();
     }
 }
+  
+  
+ 
