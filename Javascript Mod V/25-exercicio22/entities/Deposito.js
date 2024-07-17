ef6fed5e51af5585aae9eb7246355ff445fcbe7e
Elaine Santos <101003900+elainesantos-in@users.noreply.github.com>
2024-07-16 21:56:26 -0300

    exercicio

--- Javascript Mod V/25-exercicio22/entities/Deposito.js
+++ Javascript Mod V/25-exercicio22/entities/Deposito.js
@@ -1,8 +1,9 @@
 //Deposit: Deverá possuir um atributo para o valor e um atributo para a data de criação.
 
 module.exports = class Deposito {
-    constructor(valor, data){
-        this.valor = valor
-        this.data = data
+    constructor(valor) {
+      this.valor = valor;
+      this.criadoEm = new Date();
     }
-}
+  }
+  
