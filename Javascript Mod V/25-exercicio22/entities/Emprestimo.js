f7d424794b9d78289eb00e1458d4edd3ba92fd63
Elaine Santos <101003900+elainesantos-in@users.noreply.github.com>
2024-07-16 21:56:30 -0300

    exercicio

--- Javascript Mod V/25-exercicio22/entities/Emprestimo.js
+++ Javascript Mod V/25-exercicio22/entities/Emprestimo.js
@@ -1,36 +1,29 @@
 /*Emprestimo:
-
         - Deverá possuir um atributo estático privado para a taxa de juros e que possuir um getter estático para ler o atributo privado e um setter estático para definir uma nova taxa de juros a partir de uma porcentagem.
-
         - Deverá possuir os atributos para o valor do empréstimo e para a data de criação.
-
         - Também deverá possuir um atributo para as parcelas do empréstimo, sendo que o construtor deve ter como parâmetro o número de parcelas e então deve calcular as parcelas (instâncias de Installments) e armazená-las nesse atributo. */
-
-const Parcelas = require("./Parcelas")
+const Parcelas = require('./Parcelas');
 
 module.exports = class Emprestimo {
+  static #taxa = 1.05
 
-    static #taxaJurosAtual = 2.5
-
-    constructor(valorEmprestimo, data, numeroParcelas){
-        const valorParcela = Parcelas.valorParcela
-        this.valorEmprestimo = valorEmprestimo
-        this.data = data
-        this.numeroParcelas = numeroParcelas 
-        this.valorParcela = valorParcela
-        
+  constructor(valor, parcelas) {
+    this.valor = valor
+    this.parcelas = []
+    for (let i = 1; i <= parcelas; i++) {
+      this.parcelas.push(new Parcelas((valor * Emprestimo.#taxa) / parcelas, i))
     }
-    static get jurosAtual(){
-        return Emprestimo.#taxaJurosAtual;
-    }
+    this.criadoEm = new Date()
+  }
 
-    static set novJuros (novoJuros){
-        if(novoJuros !== Emprestimo.#taxaJurosAtual ){
-            return Emprestimo.#taxaJurosAtual = novoJuros
-        }
-    }
+  static get taxa() {
+    return Emprestimo.#taxa
+  }
 
-    static valorparcelas(){
-
-    }
+  static set taxa(novaTaxaPercentual) {
+    Emprestimo.#taxa = 1 + (novaTaxaPercentual / 100)
+  }
 }
+
+          
+          
